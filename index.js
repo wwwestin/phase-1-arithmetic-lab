aaedf89c31ae60f8ed4d7548fb777970f2a7caf2
Westin Humble <westin.h@gmail.com>
2022-01-11 11:15:04 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 42
+let num4 = 19
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random= Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
